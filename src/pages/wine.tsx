e1d8cd0d2046f88e9070e2bf47f82a10d310b96e
Jiyoung Hwang <jiyounghwang92@gmail.com>
2021-12-19 21:30:40 +0900

    wine 페이지 생성

---
+++ src/pages/wine.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const WinePage: NextPage = () => {
+  return (
+    <div>
+      <h1>Wine</h1>
+    </div>
+  )
+}
+
+export default WinePage;
+// 여기까지가 가장 기본적인 템플릿, TypeScript
