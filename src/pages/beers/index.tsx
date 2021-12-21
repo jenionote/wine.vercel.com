8d6f2dd4d69c689dade36a280f9ac550a45545cf
jenionote <jiyounghwang92@gmail.com>
2021-12-22 02:32:49 +0900

    wine-vercel-com-kohl.vercel.app

---
+++ src/pages/beers/index.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from "next";
+import { useBeerData } from "../../hooks/useBeerData";
+import { Beer } from "../../types/Beer";
+
+const IndexBeerePage: NextPage = () => {
+  const { data, error } = useBeerData('ale')
+
+  if (error) return <div>Failed to Loading...</div>
+  if (!data) return <div>...Loading</div>
+
+  return (
+    <div>
+      <h1>Beer</h1>
+      <main>
+        {data.map((beerData: Beer) => {
+          const { id, price, name } = beerData;
+
+          return (
+            <div key={`ale-beer-list-${id}`}>
+              <h1>{name}</h1>
+              <p>{price}</p>
+            </div>
+          )
+        })}
+      </main>
+    </div>
+  )
+}
+
+export default IndexBeerePage;
